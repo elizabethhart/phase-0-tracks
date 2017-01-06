2608edacd0fc9840795bfaf986d3c0582f70d2c0
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-06 15:40:15 -0600

    Process multiple employees loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,19 +1,4 @@
-puts "What is your name?"
-name = gets.chomp
-
-puts "How old are you?"
-age = gets.chomp .to_i
-
-puts "What year were you born?"
-year = gets.chomp.to_i 
-age_2 = 2017 - year
-
-if age == age_2
-  age = true 
-else 
-  age = false 
-end 
-
+# Method to convert yes/no responses to booleans
 def boolean_convert(str)
   if str == "yes"
     str = true 
@@ -22,24 +7,65 @@ def boolean_convert(str)
   end
 end 
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
-garlic = boolean_convert(gets.chomp) 
+# User interface
 
-puts "Would you like to enroll in the company’s health insurance? (yes/no)"
-insurance = boolean_convert(gets.chomp) 
+puts "How many employees will be processed?"
+count = gets.chomp.to_i 
 
-if age == true && (garlic || insurance)
-  if name == "Drake Cula" || name == "Tu Fang"
-    puts "Definitely a vampire."
-  else
-    puts "Probably not a vampire."
-  end
-elsif age == false && (garlic == false || insurance == false)
-  if garlic == false && insurance == false
-    puts "Almost certainly a vampire."
+x = 0
+
+while x < count
+
+  puts "What is your name?"
+  name = gets.chomp
+  
+  puts "How old are you?"
+  # Converting string response to integer 
+  age = gets.chomp .to_i
+  
+  puts "What year were you born?"
+  # Converting string response to integer 
+  year = gets.chomp.to_i 
+  # Converting birth year to age
+  age_2 = 2017 - year
+  
+  # Check to see that the ages match
+  if age == age_2
+    age = true 
   else 
-    puts "Probably a vampire."
+    age = false 
+  end 
+  
+  puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
+  garlic = boolean_convert(gets.chomp) 
+  
+  puts "Would you like to enroll in the company’s health insurance? (yes/no)"
+  insurance = boolean_convert(gets.chomp) 
+  
+  puts "Please list any allergies, type \"done\" when finished."
+  allergies = gets.chomp 
+  until allergies == "done" || allergies == "sunshine"
+    puts "Please list any allergies, type \"done\" when finished."
+    allergies = gets.chomp
   end
-else
-  puts "Results inconclusive."
+
+  # Check to see the likelihood of being a vampire
+  if age == true && (garlic || insurance)
+    if name == "Drake Cula" || name == "Tu Fang"
+      puts "Definitely a vampire."
+    else
+      puts "Probably not a vampire."
+    end
+  elsif age == false && (garlic == false || insurance == false)
+    if garlic == false && insurance == false
+      puts "Almost certainly a vampire."
+    else 
+      puts "Probably a vampire."
+    end
+  else
+    puts "Results inconclusive."
+  end
+    
+  x += 1
+  
 end
