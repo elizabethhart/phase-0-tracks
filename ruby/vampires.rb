868e8028a4863a2a5693c48a70afae4fb900fce8
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-06 15:38:34 -0600

    Add detection logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,10 +1,45 @@
 puts "What is your name?"
-vampire_name = gets.chomp
+name = gets.chomp
+
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp .to_i
+
 puts "What year were you born?"
-birth_year = gets.chomp
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic = gets.chomp
-puts "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp
+year = gets.chomp.to_i 
+age_2 = 2017 - year
+
+if age == age_2
+  age = true 
+else 
+  age = false 
+end 
+
+def boolean_convert(str)
+  if str == "yes"
+    str = true 
+  else 
+    str = false 
+  end
+end 
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
+garlic = boolean_convert(gets.chomp) 
+
+puts "Would you like to enroll in the company’s health insurance? (yes/no)"
+insurance = boolean_convert(gets.chomp) 
+
+if age == true && (garlic || insurance)
+  if name == "Drake Cula" || name == "Tu Fang"
+    puts "Definitely a vampire."
+  else
+    puts "Probably not a vampire."
+  end
+elsif age == false && (garlic == false || insurance == false)
+  if garlic == false && insurance == false
+    puts "Almost certainly a vampire."
+  else 
+    puts "Probably a vampire."
+  end
+else
+  puts "Results inconclusive."
+end
