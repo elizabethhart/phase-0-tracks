6f3428b89c617cb9473bf1bc0efd4fb60e26af0e
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-05 22:51:36 -0600

    Rearraged code according to release 5 and added comments

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,10 +1,8 @@
-puts "Would you like to decrypt or encrypt a password?"
-response = gets.chomp
-puts "What is the password?"
-password = gets.chomp
+# Method Declarations
 
 # In order to figure out how long the string is, we would utilize the .length method on the string to determine how long the loop will be run. We would take a string inside a loop and use the .next method to keep moving to the next character until we reached the end of the string.  
 
+# Encrypts any string by advancing every letter by one letter forward
 def encryption_method(string)
   x = 0
   while x < string.length
@@ -22,6 +20,7 @@ end
 
 # Ask the user for a word to encrypt. Save as a variable. Create variable to alphabet. Create while look using .length to run through the length of the entire string. Calculate position of each letter in the alphabet, and subtract one from that number. 
 
+# Decrypts any string by going backward one letter for every letter
 def decryption_method(string)
   var2 = "abcdefghijklmnopqrstuvwxyz"
   x =  0 
@@ -38,6 +37,13 @@ end
 # p decryption_method(encryption_method("swordfish"))
 
 # The encryption_method returns an encrypted string back into the decryption method when it is then decrypted back into the original string
+
+# Driver Code
+
+puts "Would you like to decrypt or encrypt a password?"
+response = gets.chomp
+puts "What is the password?"
+password = gets.chomp
 
 if response == "encrypt"
   p encryption_method(password)
