8fbdd755350a1f6f23e870e85a9cc8d68a298052
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-09 18:56:47 -0600

    Added before and after status messages

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -5,4 +5,6 @@ def hotels_available
 	yield(hotel1, hotel2)
 end
 
+puts "Before the block runs"
 hotels_available { |hotel1, hotel2| puts "The #{hotel1} is available. The #{hotel2} is not available."}
+puts "After the block runs"
