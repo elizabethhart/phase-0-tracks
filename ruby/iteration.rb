b3f00e4cde0fa595eef7bb2d5207e7616ffaa11b
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-09 20:26:30 -0600

    Add various methods for arrays and hashes

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -9,6 +9,7 @@ puts "Before the block runs"
 hotels_available { |hotel1, hotel2| puts "The #{hotel1} is available. The #{hotel2} is not available."}
 puts "After the block runs"
 
+#Release 1
 
 pets = ["dog", "cat", "snake", "bunny", "turtle"]
 
@@ -39,4 +40,71 @@ pets_names.each do |pet, name|
 end
 
 p pets_names
+
+#Release 2
+
+integer_array = [1, 2, 3, 4, 5]
+
+integer_hash = {
+	1 => "one",
+	2 => "two",
+	3 => "three",
+	4 => "four",
+	5 => "five",
+}
+
+# 1 
+
+integer_array.delete_if { |x| x < 3 }
+
+p integer_array
+
+integer_hash.delete_if { |integer, string| string.include?("o") }
+
+p integer_hash
+
+# 2
+
+integer_array.keep_if { |x| x.even? }
+
+p integer_array
+
+integer_hash.keep_if { |integer, string| string.length > 4 }
+
+p integer_hash
+
+# 3
+
+p integer_array.select { |x| x <= 4 }
+
+p integer_array
+
+p integer_hash.select { |integer, string| string.include?("o") }
+
+p integer_hash
+
+# 4
+
+p integer_array.drop_while { |x| x.odd? }
+
+p integer_array
+
+p integer_hash.reject { |integer, string| integer > 3 }
+
+p integer_hash
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
