cc32768ecde16bab1903828e32ffe22cc4f93709
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-09 19:16:16 -0600

    Add .each and .do blocks for array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -8,3 +8,26 @@ end
 puts "Before the block runs"
 hotels_available { |hotel1, hotel2| puts "The #{hotel1} is available. The #{hotel2} is not available."}
 puts "After the block runs"
+
+pets = ["dog", "cat", "snake", "bunny", "turtle"]
+
+pets_names = {
+	"dog" => "Fluffy",
+	"cat" => "Delilah",
+	"snake" => "Lucifer",
+	"bunny" => "Bugs",
+	"turtle" => "Henry"
+}
+
+pets.each do |x|
+	puts "Everybody loves #{x}s!"
+end
+
+pets.map! do |x|
+	puts x.upcase.reverse
+end
+
+pets_names.each do |pet, name|
+  puts "This #{pet} is named #{name}."
+end
+
