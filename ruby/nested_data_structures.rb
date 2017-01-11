7a320a55d7912bd46e7b6f4ac09303f4a853521e
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-10 21:01:28 -0600

    Add nested data structure about Hogwarts

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,48 @@
+Hogwarts = {
+	main_office: {
+		staff: {
+			headmaster: "Albus Dumbledore",
+			administration: 8,
+		},
+		students: {
+			in_detention: 2,
+			tardy: 1,
+		}
+	},
+	classroom_1: {
+		teachers: [
+			"Professor McGonagall",
+			"Rubeus Hagrid"
+		],
+		students: 25,
+	},
+	classroom_2: {
+		teachers: [
+			"Professor Snape"
+		],
+		furniture: [
+			"Potions Table",
+			"Desks",
+			"Bookcase",
+			"Cauldron",
+		],
+		students: 0
+	},
+	quidditch_field: {
+		players: [
+			"Oliver Wood",
+			"George Weasley",
+			"Harry Potter"
+		],
+		equipment: {
+			hoops: 6,
+			brooms: 3,
+			bludgers: 2,
+			quaffles: 1,
+			golden_snitch: 1,
+		},
+		teams: [
+			"Gryffindor"
+		]
+	}
+}
