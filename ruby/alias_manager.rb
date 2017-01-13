ab8abde7fe88592f1170a9ac40e2cb718c4fb3a9
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-12 20:35:34 -0600

    Add new methods for next vowel and next consonant

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,38 +1,49 @@
+# Change the vowels to the next vowel in the alphabet
+def next_vowel(letter)
+  vowels = ["a", "e", "i", "o", "u"]
+  new_index = vowels.index(letter) + 1
+  if new_index < vowels.length
+    letter = vowels[new_index]
+  else
+    letter = "a"
+  end
+end
+
+# Change the consonants to the next consonant in the alphabet
+def next_consonant(letter)
+  vowels = ["a", "e", "i", "o", "u"]
+  if vowels.include?(letter.next) && letter.next.length < 2
+    letter = letter.next.next
+  elsif letter.next.length == 2 
+    letter = "b"
+  else
+    letter = letter.next
+  end
+end
+
 def alias_maker(first_last)
   
 # Split the name into an array, with first name as the first value and last name as the last value
-
-  first_last = first_last.split(' ')
+  first_last = first_last.downcase.split(' ')
   
 # Swap the positions of the values in the array and join the two strings back into one string
-
   last_first = first_last.rotate.join(' ')
   
 # Split the words into individual characters
-
   new_name = last_first.split('')
   
-# Change the vowels to the next vowel in the alphabet
-  vowels = ["a", "e", "i", "o", "u"]
+  # new_name.map do |letter|
+  #   vowels = ["a", "e", "i", "o", "u"]
+  #   if vowels.include?(letter)
+  #     next_vowel(letter)
+  #   else
+  #     next_consonant(letter)
+  #   end
+  #   return new_name
+  # end
   
-  new_name.map do |letter|
-    if vowels.include?(letter)
-      new_index = vowels.index(letter) + 1
-      letter = vowels[new_index]
-    else
-      letter = letter.next!
-      if vowels.include?(letter)
-        letter = letter.next!
-      end
-    end
-  end
-  
-# Change the consonants to the next consonant in the alphabet
-
-# Join the two strings back into one string
-
+# # Join the two strings back into one string
   new_name.join(' ')
-
 end 
 
-alias_maker("Elizabeth Hart")
+alias_maker("Felicia Torres")
