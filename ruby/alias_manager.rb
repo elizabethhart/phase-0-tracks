4c9d2935ff405d6b75c3d5efcf1bfdea2ec2233e
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-12 18:38:40 -0600

    Ask for user name, same name, split name into two strings, swap names, merge strings back together, then split into an array of each letter

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,7 +1,23 @@
 # Ask the user for their name
+
+puts "What is your real name?"
+
 # Save the string as a variable
+
+user = gets.chomp
+
 # Split the name into an array, with first name as the first value and last name as the last value
-# Swap the positions of the values in the array
+
+first_last = user.split(' ')
+
+# Swap the positions of the values in the array and join the two strings back into one string
+
+last_first = first_last.rotate.join(' ')
+
+# Split the words into individual characters
+
+new_name = last_first.split('')
+
 # Change the vowels to the next vowel in the alphabet
 # Change the consonants to the next consonant in the alphabet
 # Join the two strings back into one string
