f91f6d5fced8f948e1987bbc999d421d36cfc924
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-12 18:16:14 -0600

    Pseudocode a method for creating a fake name from a spy's real name

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,7 @@
+# Ask the user for their name
+# Save the string as a variable
+# Split the name into an array, with first name as the first value and last name as the last value
+# Swap the positions of the values in the array
+# Change the vowels to the next vowel in the alphabet
+# Change the consonants to the next consonant in the alphabet
+# Join the two strings back into one string
