50e8554dab7f8d44c70e9dd9cf069762d9553057
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-14 18:20:35 -0600

    Add vowel and consonant conversions into method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,26 +1,3 @@
-# Change the vowels to the next vowel in the alphabet
-def next_vowel(letter)
-  vowels = ["a", "e", "i", "o", "u"]
-  new_index = vowels.index(letter) + 1
-  if new_index < vowels.length
-    letter = vowels[new_index]
-  else
-    letter = "a"
-  end
-end
-
-# Change the consonants to the next consonant in the alphabet
-def next_consonant(letter)
-  vowels = ["a", "e", "i", "o", "u"]
-  if vowels.include?(letter.next) && letter.next.length < 2
-    letter = letter.next.next
-  elsif letter.next.length == 2 
-    letter = "b"
-  else
-    letter = letter.next
-  end
-end
-
 def alias_maker(first_last)
   
 # Split the name into an array, with first name as the first value and last name as the last value
@@ -32,18 +9,46 @@ def alias_maker(first_last)
 # Split the words into individual characters
   new_name = last_first.split('')
   
-  # new_name.map do |letter|
-  #   vowels = ["a", "e", "i", "o", "u"]
-  #   if vowels.include?(letter)
-  #     next_vowel(letter)
-  #   else
-  #     next_consonant(letter)
-  #   end
-  #   return new_name
-  # end
+# Convert letters
   
-# # Join the two strings back into one string
-  new_name.join(' ')
+  new_name.map! do |letter|
+    vowels = ["a", "e", "i", "o", "u"]
+    if vowels.include?(letter)
+      new_index = vowels.index(letter) + 1
+      if new_index < vowels.length
+        letter = vowels[new_index]
+      else
+        letter = "a"
+      end
+    else
+      if vowels.include?(letter.next) && letter.next.length < 2
+        letter = letter.next.next
+      elsif letter.next.length == 2 
+        letter = "b"
+      elsif letter.next == "!"
+        letter = " "
+      else
+        letter = letter.next
+      end
+    end
+  end
+  
+# Join the two strings back into one string
+
+  lowercase_name = new_name.join('')
+
+# Resplit the strings into an array
+  
+  uppercase_name = lowercase_name.split(' ')
+  
+# Capitalize the names again  
+  
+  uppercase_name.map! { |name| name.capitalize }
+  
+# Join the array back into a string
+  
+  return uppercase_name.join(' ')
+  
 end 
 
-alias_maker("Felicia Torres")
+p alias_maker("Felicia Torres")
