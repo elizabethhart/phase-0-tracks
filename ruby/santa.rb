2abf37fe1c33cfa74a3497339fbfa8d1c00f1213
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-19 17:21:06 -0600

    Add initializations as driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,7 @@
 class Santa
 
 	def initialize(gender,ethnicity)
-		p "Initializing Santa instance ..."
+		puts "Initializing Santa instance ..."
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
@@ -9,16 +9,26 @@ class Santa
 	end
 
 	def speak
-		p "Ho, ho, ho! Haaaappy holidays!"
+		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
 
 	def eat_milk_and_cookies(cookie_type)
-		p "That was a good #{cookie_type}!"
+		puts "That was a good #{cookie_type}!"
 	end
 
 end
 
-jolly = Santa.new
+jolly = Santa.new("non-binary","eurasian")
 
 jolly.speak
 jolly.eat_milk_and_cookies("chocolate chip cookie")
+
+santas = []
+example_genders = ["non-binary", "female", "two-spirit"]
+example_ethnicities = ["Afrikaner", "Jewish", "Native American"]
+example_genders.length.times do |i|
+	puts "Creating a new Santa ..."
+	santas << Santa.new(example_genders[i], example_ethnicities[i])
+	puts "The new Santa is #{example_genders[i]} and #{example_ethnicities[i]}!"
+end
+
