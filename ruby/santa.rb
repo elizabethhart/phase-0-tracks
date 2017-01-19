1617679df30788713452f595a200befd20585e5d
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-19 17:00:21 -0600

    Add age attribute to initialization

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,6 +5,7 @@ class Santa
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 
 	def speak
