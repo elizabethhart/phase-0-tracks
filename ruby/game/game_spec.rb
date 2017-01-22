7158fba1cb39c962b9fd04aee369ad8336840f08
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-22 10:42:46 -0600

    Add test for storing a word as an array of characters

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -1,5 +1,10 @@
 require_relative 'game'
 
 describe Game do
+	let(:game) { Game.new }
+
+	it "stores the word given on itialization and splits it into an array containing each character" do
+		expect(game.store("simple")).to eq ["s","i","m","p","l","e"]
+	end
 	
 end
