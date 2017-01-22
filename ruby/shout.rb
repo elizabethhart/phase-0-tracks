8ac99a0137e7cd47d799d270baf36b58e69ad48a
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-21 21:39:46 -0600

    Add driver code that calls both methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -7,5 +7,13 @@ module Shout
   def self.yell_happily(words)
   	words + "!!!" + " :)"
   end
-  
+
 end
+
+# Driver Code
+
+# Call method for yelling angrily
+p Shout.yell_angrily("Darn")
+
+# Call method for yelling happily
+p Shout.yell_happily("Yay")
