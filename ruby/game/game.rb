d6e62e2e7ec3dd836b45afd7a4abb047252f3eb2
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-22 10:10:36 -0600

    Add pseudocode from release 1

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -4,3 +4,20 @@ class Game
 	end
 
 end
+
+# Word Guessing Game
+# Allow the user to enter a word
+# Save the word 
+# Count the characters in the word
+# Initialize a counter variable based on how long the word is
+# Run the following loop until the the counter reaches the maximum length
+	# Ask the next user to guess a letter
+	# Check if that letter is in the word
+	# If the letter is in the word
+		# Print the letter where it is in the word, with underscores everywhere else
+	# Or print guess again
+	# Add one to the counter variable
+# If the user guesses all of the letters correctly before the limited guesses are over
+	#Print a congratulations
+# Or if they did not get the word
+	#Print a taunting message
