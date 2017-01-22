df7c1ea3a68d4242851d85a96c823ca144cc22d2
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-22 17:29:59 -0600

    Changed formatting of tests to run without the need for pre-existing arrays

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -4,23 +4,26 @@ describe Game do
 	let(:game) { Game.new }
 
 	it "stores the word given on itialization and splits it into an array containing each character" do
-		expect(game.store("simple")).to eq ["s","i","m","p","l","e"]
+		expect(game.store("test")).to eq ["t","e","s","t"]
 	end
 
 	it "creates an array of underscores based on length of word" do
-		expect(game.create_empty_array("simple")).to eq ["_ ","_ ","_ ","_ ","_ ","_ "]
+		expect(game.create_empty_array("test")).to eq ["_ ","_ ","_ ","_ "]
 	end	
 
 	it "checks if the guessed letter is the word array" do
-		expect(game.check_word("p")).to eq true
+		game.store("test")
+		expect(game.check_word("t")).to eq true
 	end
 	
 	it "assigns the letter to the empty array and print string" do
-		expect(game.assign_letter("p")).to eq ["_ ","_ ","_ ","p ","_ ","_ "]
+		game.store("test")
+		game.create_empty_array("test")
+		expect(game.assign_letter("t")).to eq ["t ","_ ","_ ","t "]
 	end	
 
 	it "converts array to string and removes all whitespace" do
-		expect(game.array_to_string(["s","i","m","p","l","e"])).to eq "simple"
+		expect(game.array_to_string(["t","e","s","t"])).to eq "test"
 	end
 
 end
