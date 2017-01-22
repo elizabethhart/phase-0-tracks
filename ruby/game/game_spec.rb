a3c6b27ed55092100a2defc66fc8d3cd52764a3a
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-22 10:47:43 -0600

    Add test for checking if a letter is in the word

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -6,5 +6,9 @@ describe Game do
 	it "stores the word given on itialization and splits it into an array containing each character" do
 		expect(game.store("simple")).to eq ["s","i","m","p","l","e"]
 	end
+
+	it "checks if the guessed letter is the word array" do
+		expect(game.check_word("p")).to eq "_ _ _ p _ _"
+	end
 	
 end
