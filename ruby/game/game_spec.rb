2ff7acbbdcd0a44d0474bb9c868e5ecd3a472d1d
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-22 11:04:16 -0600

    Add tests for creating an empty array of underscores and checking guessed letter

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -7,6 +7,10 @@ describe Game do
 		expect(game.store("simple")).to eq ["s","i","m","p","l","e"]
 	end
 
+	it "creates an array of underscores based on length of word" do
+		expect(game.create_empty_array("simple")).to eq ["_ ","_ ","_ ","_ ","_ ","_ "]
+	end	
+
 	it "checks if the guessed letter is the word array" do
 		expect(game.check_word("p")).to eq "_ _ _ p _ _"
 	end
