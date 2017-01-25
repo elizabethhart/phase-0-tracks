4b8fbfcf88a7100d259697368705f2de56996dbb
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-25 16:14:52 -0600

    Psuedocode for a function that takes a string as a parameter and reverse the string

---
+++ js/explore.js
@@ -0,0 +1,15 @@
+// Psuedocode for a function that takes a string as a parameter and reverse the string
+
+// Assign a variable to the string
+
+// Separate the string into an array with each character in an index
+
+// Determine the length of the array
+
+// For each character
+
+	// Starting with the first, assign it the contents of the last index in the array
+	// Subtract one from the last index, assign its contents to the next character in the array
+	// Repeat until the index value reaches 0
+
+// Add all characters back into one string
