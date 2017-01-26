3cf5698d8e2673fa823f376bfa679059edc80eaf
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-26 16:44:10 -0600

    Added algorithm for a function that takes an array of words or phrases and returns the longest word or phrase

---
+++ js/algos.js
@@ -0,0 +1,12 @@
+// write a function that takes an array of words or phrases and returns the longest word or phrase 
+
+// for each string in the array 
+		// check if the length of string is less than the following strings in the array
+		// if so 
+			// write a new variable with the first string
+			// then reassign the first string the value of the longer string
+			// then reassign the longer string the value of the first string
+			// (this will move the longest values to the front of the array)
+		// else 
+			// rerun the loop for the next value in the array
+// return the first value of the array
