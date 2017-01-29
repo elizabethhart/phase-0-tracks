c74909ec0f68cf9f8ff148460c99ed928e030795
Elizabeth Hart <elizabethwhart@gmail.com>
2017-01-29 13:38:12 -0600

    Rewrite longestString function to return correct string

--- js/algos.js
+++ js/algos.js
@@ -1,27 +1,25 @@
 // write a function that takes an array of words or phrases and returns the longest word or phrase 
 
 // for each string in the array 
-		// check if the length of string is less than the following strings in the array
+		// check if the length of first string is less than the following strings in the array
 		// if so 
-			// write a new variable with the first string
-			// then reassign the first string the value of the longer string
-			// then reassign the longer string the value of the first string
-			// (this will move the longest values to the front of the array)
+			// write a new variable with the longest string string
 		// else 
 			// rerun the loop for the next value in the array
 // return the first value of the array
 
 function longestString(arr) {
+  var maxIndex = 0;
+  var maxLength = arr[maxIndex].length;
 	for (var i = 0; i < arr.length; i++) {
-		for (var x = i + 1; x < arr.length; x++) {
-			if (arr[i] < arr[x]) {
-				var placeholder = arr[i];
-				arr[i] = arr[x];
-				arr[x] = arr[placeholder];
-			}
-		}
-	}
-	return arr[0];
+	  var nextLength = arr[i].length;
+	  if (maxLength < nextLength) {
+	    maxIndex = i;
+	    maxLength = arr[i].length;
+	    maxString = arr[i];
+	  }
+  }
+  return maxString;
 }
 
 // write a function that takes two objects and checks to see if the objects share at least one key value pair
