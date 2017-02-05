c7f6e7817b752a8b5c24e4ae4d13788ce0b18322
Elizabeth Hart <elizabethwhart@gmail.com>
2017-02-05 16:05:21 -0600

    Create grade log table and method for logging grades

--- databases/grade_log/grade_log.rb
+++ databases/grade_log/grade_log.rb
@@ -6,3 +6,22 @@ require 'faker'
 # create SQLite3 database
 
 db = SQLite3::Database.new("grades.db")
+
+# create table
+
+grades_log = <<-SQL 
+	CREATE TABLE IF NOT EXISTS grades(
+		id INTEGER PRIMARY KEY,
+		student_name VARCHAR(255),
+		assignment_name VARCHAR(255),
+		grade INT
+	)
+SQL
+
+db.execute(grades_log)
+
+# method for logging grades
+
+def log_grade(db, student_name, assignment_name, grade)
+	db.execute("INSERT INTO grades (student_name, assignment_name, grade) VALUES (?, ?, ?)", [student_name, assignment_name, grade])
+end
