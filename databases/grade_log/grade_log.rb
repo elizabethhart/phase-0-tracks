4774b9597873a6fc698fce51c5d64ab848efcbd0
Elizabeth Hart <elizabethwhart@gmail.com>
2017-02-05 18:35:57 -0600

    Refactor log_grade method and add view_grades and question_user method

--- databases/grade_log/grade_log.rb
+++ databases/grade_log/grade_log.rb
@@ -21,25 +21,50 @@ db.execute(grades_log)
 
 # method for logging grades
 
-def log_grade(db, student_name, assignment_name, grade)
-	db.execute("INSERT INTO grades (student_name, assignment_name, grade) VALUES (?, ?, ?)", [student_name, assignment_name, grade])
+def log_grade
+	db = SQLite3::Database.new("grades.db")
+	complete = false
+	until complete
+		puts "Please enter the student's name, or type done to exit"
+		student_name = gets.chomp
+		if student_name == "done"
+			complete = true
+		else
+			puts "Please enter the assignment name"
+			assignment_name = gets.chomp
+			puts "Please enter the grade"
+			grade = gets.chomp
+			db.execute("INSERT INTO grades (student_name, assignment_name, grade) VALUES (?, ?, ?)", [student_name, assignment_name, grade])
+		end
+	end
+end
+
+def view_grades
+	db = SQLite3::Database.new("grades.db")
+	db.execute("SELECT * FROM grades")
+end
+
+def question_user(response)
+	if response == "y"
+		log_grade
+	elsif response == "n"
+		puts "Do you want to view your previously entered grades? (y/n)"
+		response2 = gets.chomp
+		if response2 == "y"
+			p view_grades
+		else
+			puts "Goodbye"
+		end
+	else
+		puts "This is an invalid response"
+	end
 end
 
 # driver code for users adding data
 
-response = ""
-until response == "done"
-	puts "Please enter the student's name, or type done to exit"
-	student_name = gets.chomp
-	if student_name == "done"
-		response = "done"
-	else
-		puts "Please enter the assignment name"
-		assignment_name = gets.chomp
-		puts "Please enter the grade"
-		grade = gets.chomp
-		log_grade(db, student_name, assignment_name, grade)
-	end
-end
+puts "Do you want to enter grades? (y/n)"
+response = gets.chomp
+question_user(response)
+
 
 
