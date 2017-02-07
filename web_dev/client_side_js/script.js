c01584d527220817d7cef895da6fd343bf9afcea
Elizabeth Hart <elizabethwhart@gmail.com>
2017-02-06 19:29:24 -0600

    Add event listener to project

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,14 +1,20 @@
 console.log("Hello!");
 
-// function disappearingPhoto(event) {
-// 	var photo = document.getElementById("examples");
-// 	photo.hidden = true;
-// }
 
-// var photo = document.getElementById("examples");
-// photo.addEventListener("click", disappearingPhoto);
+function disappearingPhoto(event) {
+	var photoDiv = document.getElementById("examples");
+	var photo = document.getElementsByTagName("img");
+	var photo1 = photo[0];
+	var photo2 = photo[1];
+	var photo3 = photo[2];
+	photoDiv.removeChild(photo1);
+	photoDiv.removeChild(photo2);
+	photoDiv.removeChild(photo3);
+	var newParagraph = document.createElement("P");  
+	var newText = document.createTextNode("The photos disappeared!");
+	newParagraph.appendChild(newText); 
+	document.getElementById("examples").appendChild(newParagraph);
+}
 
-var newParagraph = document.createElement("P");  
-var newText = document.createTextNode("We added a paragraph!");
-newParagraph.appendChild(newText); 
-document.getElementById("examples").appendChild(newParagraph);
+var photo = document.getElementById("examples");
+photo.addEventListener("click", disappearingPhoto);
